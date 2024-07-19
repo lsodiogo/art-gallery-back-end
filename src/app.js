f0ed6d65cdecd564f52a4dab99bf143380d05b28
Diogo Lima <75363687+diogooslima@users.noreply.github.com>
2024-07-19 12:00:27 +0200

    back-end project created

---
+++ src/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+const secretKey = process.env.SECRET_KEY;
+const corsOptions = {
+   origin: process.env.BASE_DOMAIN,
+   credentials: true
+};
+
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser(secretKey));
+
+
+app.use("/home", );
+
+
+app.listen(port, function() {
+   console.log(`Listening on ${port}`);
+});
